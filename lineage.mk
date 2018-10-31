2500262b1aeebc60b6d3fc129ee63e359e3d14db
K3NG_Natan <jchan1531@gmail.com>
2018-10-31 12:16:44 +0000

    Fix Google Cert

--- lineage.mk
+++ lineage.mk
@@ -13,13 +13,17 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := PixelV2
-PRODUCT_NAME := lineage_PixelV2
+PRODUCT_DEVICE := Pixel V2
+PRODUCT_NAME := lineage_Pixel V2
 PRODUCT_BRAND := lava
-PRODUCT_MODEL := Lava PixelV2
+PRODUCT_MODEL := Lava Pixel V2
 PRODUCT_MANUFACTURER := lava
 
 PRODUCT_GMS_CLIENTID_BASE := android-lava
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="Lava/full_lava35p_sp31_lte_V2/lava35p_sp31_lte_V2:6.0/MRA58K/1481601836:user/release-keys" \
+    PRIVATE_BUILD_DESC="full_lava35p_sp31_lte_V2-user 6.0 MRA58K 1481601836 release-keys"
 
 # Available languages
 PRODUCT_LOCALES := en_US en_GB ru_RU uk_UA tr_TR sk_SK vi_VN fr_FR ar_EG th_TH
